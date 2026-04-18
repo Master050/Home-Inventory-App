7c6b8916fd9012c02471089911d42af1e25eaee5
Usuario <usuario@home.com>
2026-04-18 13:38:05 -0300

    style: balance table columns with dual-auto distribution

--- frontend/src/pages/Inventory.jsx
+++ frontend/src/pages/Inventory.jsx
@@ -384,8 +384,8 @@ export default function Inventory() {
           <table className="w-full text-sm text-left table-fixed">
             <colgroup>
               <col style={{ width: "50px" }} />
-              <col style={{ width: "350px" }} />
-              <col style={{ width: "160px" }} />
+              <col style={{ width: "auto" }} />
+              <col style={{ width: "180px" }} />
               <col style={{ width: "80px" }} />
               <col style={{ width: "90px" }} />
               <col style={{ width: "90px" }} />
